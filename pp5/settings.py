ad072ee5d8b71a72503867674e01ac5596023990
NB1355 <nahal5513@gmail.com>
2023-07-26 22:29:51 +0000

    ADD finish homepage header and CSS

--- pp5/settings.py
+++ pp5/settings.py
@@ -159,3 +159,12 @@ LOGIN_REDIRECT_URL = '/success'
 
 
 # WSGI_APPLICATION = 'boutique_ado.wsgi.application'  ?????????????????????????????? source
+
+
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
